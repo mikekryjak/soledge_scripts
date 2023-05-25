ba4aebbdb0cf293b630cbecbbdac3ec586c539e0
Mike Kryjak <mikekryjak@gmail.com>
2023-05-25 16:14:27 +0100

    1d wfluxes: fix paths, add dataframe output

--- plot1d_wall_fluxes.py
+++ plot1d_wall_fluxes.py
@@ -38,6 +38,7 @@ import h5py
 import os
 import numpy											as np
 import matplotlib
+import pandas as pd
 matplotlib.use('TkAgg')
 import matplotlib.pyplot							as pyp
 from matplotlib.backends.backend_pdf	import PdfPages
@@ -52,18 +53,18 @@ from files.load_eirene_triangles					import load_eirene_triangles
 # This routine plots ne/ni and Te/Ti ionization and gas pressure on eirene mesh
 #==============================================================================
 
-def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0, one_plot=0, no_samex=0, save="none"):
+def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0, one_plot=0, no_samex=0, no_plot=0, save="none"):
 
 	print("plot1d_wall_fluxes")
 
-#	Read reference parameters
+	#	Read reference parameters
 
-	RefPar = load_refpar_file(path+"Results/")
+	RefPar = load_refpar_file(os.path.join(path, "Results"))
 	Ions = load_ions_list(path)
 
-#	Read wall
+	#	Read wall
 
-	if_mesh = h5py.File(str(path)+"/mesh.h5", "r")
+	if_mesh = h5py.File(os.path.join(path, "mesh.h5"), "r")
 
 	try:
 		Rwall	= h5_read(if_mesh, "walls/wall1/R")
@@ -74,16 +75,16 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 
 	if_mesh.close()
 
-#	Find wall intersection along line
+	#	Find wall intersection along line
 
 
-	Eirene = load_eirene_triangles(path+"triangles.h5")
+	Eirene = load_eirene_triangles(os.path.join(path, "triangles.h5"))
 	ZeroTriangle, ZeroSide, Ri, Zi = get_wall_triangle(Eirene, rz0_line=rz0_line, theta_line=theta_line, no_plot=1, no_print=1)
 	Eirene = 0
 
-#	Read Eirene mesh
+	#	Read Eirene mesh
 
-	if_tri	 = h5py.File(str(path)+"/triangles.h5", "r")
+	if_tri	 = h5py.File(os.path.join(path, "triangles.h5"), "r")
 
 	TriKnots = h5_read(if_tri,"triangles/tri_knots")
 	TriKnots = TriKnots - 1 										#Matlab/Fortan to python indexes
@@ -94,11 +95,11 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 
 
 	try:
-		AFluxF	 = np.loadtxt(path+"soledge2D.atoms_fluxes_wall_1", dtype='f8')
-#            		[0:iTri, 1:iSide, 2:iProp, 3:dlWall, 
-#					 4:Flux_Atom_Incident, 			5:Flux_Mol_Incident,
-#					 6:Flux_Atom_Emitted_from_Ion, 	7:Flux_Mol_Emitted_from_Ion,
-#					 8:Flux_Atom_Emitted_from_Atom, 9:Flux_Mol_Emitted_from_Atom]
+		AFluxF	 = np.loadtxt(os.path.join(path, "soledge2D.atoms_fluxes_wall_1"), dtype='f8')
+	#            		[0:iTri, 1:iSide, 2:iProp, 3:dlWall, 
+	#					 4:Flux_Atom_Incident, 			5:Flux_Mol_Incident,
+	#					 6:Flux_Atom_Emitted_from_Ion, 	7:Flux_Mol_Emitted_from_Ion,
+	#					 8:Flux_Atom_Emitted_from_Atom, 9:Flux_Mol_Emitted_from_Atom]
 	except:
 		print("\tError: Not found "+ path+"soledge2D.atoms_fluxes_wall_1")
 		exit()
@@ -108,14 +109,14 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 	AFluxF  = 0
 
 	Vertex	= np.array([[0,1],[1,2],[2,0]], dtype='i4')
-	iTri	= iTri.astype(np.int)
-	iSide	= iSide.astype(np.int)
+	iTri	= iTri.astype(int)
+	iSide	= iSide.astype(int)
 
 	iZero = np.where(iTri == ZeroTriangle); iZero = iZero[0]
 	if(len(iZero) != 1):
 		print("\tError: not found wall triangle intersection with line")
 		exit()
-	
+
 	iZero  = iZero[0]
 	zOrder = np.arange(len(iTri))
 	zOrder = np.append(zOrder[iZero:], zOrder[:iZero]) 
@@ -129,11 +130,11 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 	WallR	= 0.5*(R[TriKnots[iTri,Vertex[iSide,0]]]+R[TriKnots[iTri,Vertex[iSide,1]]])
 	WallZ	= 0.5*(Z[TriKnots[iTri,Vertex[iSide,0]]]+Z[TriKnots[iTri,Vertex[iSide,1]]])
 	WalldL  = np.sqrt((R[TriKnots[iTri,Vertex[iSide,0]]]-R[TriKnots[iTri,Vertex[iSide,1]]])**2 + \
-					  (Z[TriKnots[iTri,Vertex[iSide,0]]]-Z[TriKnots[iTri,Vertex[iSide,1]]])**2)
+						(Z[TriKnots[iTri,Vertex[iSide,0]]]-Z[TriKnots[iTri,Vertex[iSide,1]]])**2)
 
 
 	try:
-		FluxiF	 = np.loadtxt(path+"soledge2D.ion_fluxes_wall_1", dtype='f8')
+		FluxiF	 = np.loadtxt(os.path.join(path,"soledge2D.ion_fluxes_wall_1"), dtype='f8')
 #            		[0:iTri, 1:iSide, 2:iProp, 3:dlSurf, 
 #					 4:Flux_Ion_Incident, 			5:Flux_Atom_Emitted_from_Ion]
 	except:
@@ -145,14 +146,14 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 	#Reads neutral flux
 	FluxiN=[]
 	try:
-		FluxiN.append(np.loadtxt(path+"soledge2D.atoms_fluxes_wall_1", dtype='f8'))
+		FluxiN.append(np.loadtxt(os.path.join(path, "soledge2D.atoms_fluxes_wall_1"), dtype='f8'))
 #            		[0:iTri, 1:iSide, 2:iProp, 3:dlWall, 
 #					 4:Flux_Atom_Incident, 			5:Flux_Mol_Incident,
 #					 6:Flux_Atom_Emitted_from_Ion, 	7:Flux_Mol_Emitted_from_Ion,
 #					 8:Flux_Atom_Emitted_from_Atom, 9:Flux_Mol_Emitted_from_Atom]
 		i=2
 		while(os.path.isfile('./soledge2D.atoms_fluxes_wall_'+str(i))):
-			FluxiN.append(np.loadtxt(path+"soledge2D.atoms_fluxes_wall_"+str(i), dtype='f8'))
+			FluxiN.append(np.loadtxt(os.path.join(path,"soledge2D.atoms_fluxes_wall_"+str(i)), dtype='f8'))
 			i=i+1
 	except:
 		print("\tError: Not found "+ path+"soledge2D.atoms_fluxes_wall_1")
@@ -169,24 +170,24 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 	try:
 		details = False
 		if(details):
-			EFluxF	 = np.loadtxt(path+"soledge2D.energy_fluxes_details_1", dtype='f8', comments="%")
-#			[0:dlWall, 
-#			1:Flux_Energy_incident_Electron, 	2:Flux_Energy_incident_Ions
-#			3:Flux_Energy_Total,				4:Flux_Energy_incident_Atoms,
-#			5:Flux_Energy_Radiation_Atoms,		6:Flux_Energy_Rad_Recombination,
-#			7:Flux_Energy_Recombination_in_Wall,8:Flux_Energy_incident_Molecules]
+			EFluxF	 = np.loadtxt(os.path.join(path, "soledge2D.energy_fluxes_details_1"), dtype='f8', comments="%")
+	#			[0:dlWall, 
+	#			1:Flux_Energy_incident_Electron, 	2:Flux_Energy_incident_Ions
+	#			3:Flux_Energy_Total,				4:Flux_Energy_incident_Atoms,
+	#			5:Flux_Energy_Radiation_Atoms,		6:Flux_Energy_Rad_Recombination,
+	#			7:Flux_Energy_Recombination_in_Wall,8:Flux_Energy_incident_Molecules]
 			FluxEe		= EFluxF[zOrder,1]*1.e-6
 			FluxEi		= EFluxF[zOrder,2]*1.e-6
 			FluxETot	= EFluxF[zOrder,3]*1.e-6
 			FluxERadTot	= EFluxF[zOrder,5]*1.e-6
 
 		else:
-			EFluxF	 = np.loadtxt(path+"soledge2D.energy_fluxes_1", dtype='f8', comments="%")
-#           [0:iTri, 1:iSide, 2:iProp, 3:dlWall, 
-#			 4:Flux_Energy_incident_Ions_EIRENE	5:Flux_Energy_incident_Electron,
-#			 6:Flux_Energy_incident_Ions		7:Flux_Energy_Total,				
-#			 8:Flux_Energy_incident_Atoms,		9:Flux_Energy_Radiation		
-#			 10:Area]
+			EFluxF	 = np.loadtxt(os.path.join(path, "soledge2D.energy_fluxes_1"), dtype='f8', comments="%")
+	#           [0:iTri, 1:iSide, 2:iProp, 3:dlWall, 
+	#			 4:Flux_Energy_incident_Ions_EIRENE	5:Flux_Energy_incident_Electron,
+	#			 6:Flux_Energy_incident_Ions		7:Flux_Energy_Total,				
+	#			 8:Flux_Energy_incident_Atoms,		9:Flux_Energy_Radiation		
+	#			 10:Area]
 			FluxEe		= EFluxF[zOrder,5]*1.e-6
 			FluxEi		= EFluxF[zOrder,6]*1.e-6
 			FluxETot	= EFluxF[zOrder,7]*1.e-6
@@ -234,64 +235,64 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 	
 
 #	Prepare for plotting
+	if no_plot == 0:
+		if(save == "pdf"):	pdf = PdfPages("plot1d_wall_fluxes_t={:.3f}.".format(RefPar.time)+save)   #pdf in one file only
 
-	if(save == "pdf"):	pdf = PdfPages("plot1d_wall_fluxes_t={:.3f}.".format(RefPar.time)+save)   #pdf in one file only
-
-	Fig = []
-	Ax  = []
-	Titles	   = ["$\Gamma_{TotI}="+sTFluxi+"\ (*10^{22})$",
-				  "$\Gamma_{TotE}="+sTFluxETot+",\ \Gamma_{Tot,ei}="+sTFluxETotei+",\ \Gamma_{Tot,Rad}="+sTFluxERadTot+"\ (MW)$",
-				  "$\Gamma_{TotD}="+sTaFlux+",\ \Gamma_{TotD2}="+sTmFlux+"\ (*10^{22})$"]
-	xLabels    = ["$wall\ (m)$", "$wall\ (m)$","$wall\ (m)$"]
-	yLabels    = ["$\Gamma_p\ (*10^{22}\ m^{-3})$", "$\Gamma_E\ (MW/m^2)$","$\Gamma_{n}\ (*10^{22}\ m^{-3})$"]
+		Fig = []
+		Ax  = []
+		Titles	   = ["$\Gamma_{TotI}="+sTFluxi+"\ (*10^{22})$",
+					"$\Gamma_{TotE}="+sTFluxETot+",\ \Gamma_{Tot,ei}="+sTFluxETotei+",\ \Gamma_{Tot,Rad}="+sTFluxERadTot+"\ (MW)$",
+					"$\Gamma_{TotD}="+sTaFlux+",\ \Gamma_{TotD2}="+sTmFlux+"\ (*10^{22})$"]
+		xLabels    = ["$wall\ (m)$", "$wall\ (m)$","$wall\ (m)$"]
+		yLabels    = ["$\Gamma_p\ (*10^{22}\ m^{-3})$", "$\Gamma_E\ (MW/m^2)$","$\Gamma_{n}\ (*10^{22}\ m^{-3})$"]
 
 
-	if(one_plot != 1):
-		Fig.append(pyp.figure())
-		for i in range(len(yLabels)):
-			if((i == 0) or (no_samex != 0)):	Ax.append(Fig[0].add_subplot(len(yLabels),1,i+1))
-			else:								Ax.append(Fig[0].add_subplot(len(yLabels),1,i+1, sharex = Ax[0]))
-
-		Fig[-1].tight_layout(pad=2., w_pad=3., h_pad=3.)
-	else:
-		for i in range(len(yLabels)):
+		if(one_plot != 1):
 			Fig.append(pyp.figure())
+			for i in range(len(yLabels)):
+				if((i == 0) or (no_samex != 0)):	Ax.append(Fig[0].add_subplot(len(yLabels),1,i+1))
+				else:								Ax.append(Fig[0].add_subplot(len(yLabels),1,i+1, sharex = Ax[0]))
 
-			Ax.append(Fig[i].add_subplot(111))
-			Fig[-1].tight_layout()
-		
-	for i in range(len(xLabels)):
-		Ax[i].autoscale(enable=True, axis='both', tight=True)
-		if(i==0):	Ax[i].set_title(os.path.basename(os.path.abspath(path))+" @ t={:.3f} s: ".format(RefPar.time)+Titles[i])
-		else:		Ax[i].set_title(Titles[i])
-		Ax[i].set_xlabel(xLabels[i])
-		Ax[i].set_ylabel(yLabels[i])
-		if(log_scale == 0):
-			Ax[i].set_yscale('linear')
+			Fig[-1].tight_layout(pad=2., w_pad=3., h_pad=3.)
 		else:
-			Ax[i].set_yscale('log')
+			for i in range(len(yLabels)):
+				Fig.append(pyp.figure())
 
-	#	Plot parameters
+				Ax.append(Fig[i].add_subplot(111))
+				Fig[-1].tight_layout()
+			
+		for i in range(len(xLabels)):
+			Ax[i].autoscale(enable=True, axis='both', tight=True)
+			if(i==0):	Ax[i].set_title(os.path.basename(os.path.abspath(path))+" @ t={:.3f} s: ".format(RefPar.time)+Titles[i])
+			else:		Ax[i].set_title(Titles[i])
+			Ax[i].set_xlabel(xLabels[i])
+			Ax[i].set_ylabel(yLabels[i])
+			if(log_scale == 0):
+				Ax[i].set_yscale('linear')
+			else:
+				Ax[i].set_yscale('log')
 
-	pl = []
-	pl.append(Ax[len(pl)].plot(WallL, Fluxi, label="$\Gamma_i$"))
+		#	Plot parameters
 
-	pl.append(Ax[len(pl)].plot(WallL, FluxETot, label="$\Gamma_{E,tot}$"))
+		pl = []
+		pl.append(Ax[len(pl)].plot(WallL, Fluxi, label="$\Gamma_i$"))
 
-	Ax[len(pl)-1].plot(WallL, FluxERadTot, label="$\Gamma_{E,Rad}$")
-	Ax[len(pl)-1].plot(WallL, FluxETotei, label="$\Gamma_{E,Par}$")
-	Ax[len(pl)-1].plot(WallL, FluxEe, label="$\Gamma_{E,e}$")
-	Ax[len(pl)-1].plot(WallL, FluxEi, label="$\Gamma_{E,i}$")
+		pl.append(Ax[len(pl)].plot(WallL, FluxETot, label="$\Gamma_{E,tot}$"))
 
-	pl.append(Ax[len(pl)].plot(WallL, fluxN[0], label="$\Gamma_{D}$"))
-	Ax[len(pl)-1].plot(WallL, fluxN[1], label="$\Gamma_{D2}$")
-	for i in range(len(FluxiN)-1):
-		Ax[len(pl)-1].plot(WallL, fluxN[i+2]*1e2, label="$\Gamma_{imp}*100$")
+		Ax[len(pl)-1].plot(WallL, FluxERadTot, label="$\Gamma_{E,Rad}$")
+		Ax[len(pl)-1].plot(WallL, FluxETotei, label="$\Gamma_{E,Par}$")
+		Ax[len(pl)-1].plot(WallL, FluxEe, label="$\Gamma_{E,e}$")
+		Ax[len(pl)-1].plot(WallL, FluxEi, label="$\Gamma_{E,i}$")
 
-	for i in range(len(Ax)):
-		Ax[i].legend(fontsize='medium')
+		pl.append(Ax[len(pl)].plot(WallL, fluxN[0], label="$\Gamma_{D}$"))
+		Ax[len(pl)-1].plot(WallL, fluxN[1], label="$\Gamma_{D2}$")
+		for i in range(len(FluxiN)-1):
+			Ax[len(pl)-1].plot(WallL, fluxN[i+2]*1e2, label="$\Gamma_{imp}*100$")
 
-	for figure in Fig:  figure.patch.set_facecolor('white')
+		for i in range(len(Ax)):
+			Ax[i].legend(fontsize='medium')
+
+		for figure in Fig:  figure.patch.set_facecolor('white')
 
 	if(save != "none"):
 		if(save == "csv"):
@@ -306,6 +307,7 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 
 			Header = "R (m), Z (m), L (m), i_flux (m^-3), Etot (W/m^2), FluxERadTot (W/m^2)"
 			np.savetxt("wall_fluxes_{:s}_t={:.3f}.csv".format(os.path.basename(os.path.abspath(path)),RefPar.time),  save_cvs, header=Header, delimiter=",", fmt="%15.7e", comments="")
+
 
 		else:
 			for i in range(len(Fig)):
@@ -322,7 +324,22 @@ def plot1d_wall_fluxes(path="./", rz0_line = [0.,0.], theta_line=0., log_scale=0
 
 	if(save == "pdf"):	pdf.close()
 
+	# MK save outputs to dataframe
+	df = pd.DataFrame()
+	df["R"] = WallR    # Element R coord
+	df["Z"] = WallZ    # Element Z coord
+	df["L"] = WallL    # Element length
+	df["pflux_i"] = Fluxi   # Ion particle flux
+	df["hflux_tot"] = FluxETot
+	df["hflux_totrad"] = FluxERadTot
+	df["hflux_par_tot"] = FluxETotei
+	df["hflux_par_e"] = FluxEe
+	df["hflux_par_i"] = FluxEi
+	df["hflux_a"] = fluxN[0]   # Neutral atom hflux
+	df["hflux_m"] = fluxN[1]   # Neutral molecule hflux
+	df["hflux_n"] = fluxN[0] + fluxN[1]    # Total neutral hflux
+
 	print("plot1d_wall_fluxes: Completed")
 
-	return
+	return df
 
